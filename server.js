6ba5a56d143cf4cea6d97ecb2b63eb2afa5233ac
Brady Camp <bradyjcamp@gmail.com>
2022-02-23 14:05:36 -0500

    added appropriate error handling

--- server.js
+++ server.js
@@ -2,14 +2,13 @@
 
 console.log('Hello World, from our first server!');
 
-const express = require('express');
-// must use require instead of import
 require('dotenv').config();
-const cors = require('cors');
+
+const express = require('express');
 const app = express();
+const cors = require('cors');
 app.use(cors());
 const weather = require('./data/weather.json');
-const res = require('express/lib/response');
 const PORT = process.env.PORT || 3002;
 
 //creating basic default route for proof of life
@@ -24,13 +23,13 @@ app.get('/weather', (request, response) => {
     // let lon = request.query.lon;
     console.log(searchQuery);
     let cityObj = weather.find((city) => city.city_name === searchQuery);
-    // let selectedCity = new Forecast(cityObj);
     // console.log(selectedCity);
     const weatherArr = cityObj.data.map((day) => new Forecast(day));
     // console.log(weatherArr);
     response.send(weatherArr);
   } catch (error) {
-    res.status(500).send('500, something went wrong');
+    throw new Error('weather not working');
+    // response.status(500).send('500, something went wrong');
   }
 });
 
@@ -44,9 +43,14 @@ class Forecast {
   }
 }
 
+app.use(errorhandler);
+function errorhandler(error, request, response, next){
+  response.status(500).send(error.message);
+}
+
 app.get('*', (request, response) => {
-  response.send(
-    'Not sure what you are looking for, but that route does not exist'
+  response.status(404).send(
+    'Route does not exist'
   );
 });
 
